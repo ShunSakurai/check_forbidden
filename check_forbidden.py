6e3573549bfe39d63b147e2f46ad15bd298b7c5a
ShunSakurai <justmyway.yui@gmail.com>
2016-04-08 16:18:42 +0900

    Divide the script part and the UI part

--- check_forbidden.py
+++ check_forbidden.py
@@ -3,13 +3,9 @@ cd dropbox/codes/check_forbidden
 py check_forbidden.py
 '''
 
-import csv
-import zipfile
-import os
+import cf_scripts
 import tkinter
 import tkinter.filedialog
-import re
-from time import sleep
 
 tk_F = tkinter.Frame()
 
@@ -78,101 +74,9 @@ help_csv.bind('<Enter>', show_format)
 help_csv.bind('<Leave>', hide_format)
 
 
-def str_to_ls(x):
-    l_f_s = [i.strip('\'') for i in x.strip('[]').split(', ')]
-    return l_f_s
-
-
-def check():
-    print('-' * 40)
-    fn1_list_raw = var_bl.get().strip('()').split(', ')
-    fn1_list = [i.strip().strip(', ').strip('"').strip("'") for i in fn1_list_raw]
-    fn2 = var_csv.get()
-    f2 = open(fn2, encoding='utf-8')
-    f3w = []
-    list_delete = []
-    list_found = []
-    regex_pattern = re.compile('<target xml:space="preserve">.*?</target>')
-
-    for fn1 in fn1_list:
-        if fn1[-5:] == 'mqxlz':
-            path_export = fn1.rsplit(r'.', 1)[0]+'_export'
-            list_delete.append(path_export)
-            z1 = zipfile.ZipFile(fn1)
-            fn1_actual = z1.extract('document.mqxliff', path=path_export)
-        else:
-            fn1_actual = fn1
-
-        fn1_actual = fn1_actual.replace('\\', '/')
-        f1 = open(fn1_actual, encoding='utf-8')
-        f1r_raw = f1.read()
-        f1r = regex_pattern.findall(f1r_raw)
-
-        f3w.append([fn1])
-        print(fn1)
-
-        f2.seek(0)
-        f2r = csv.reader(f2)
-        for row in f2r:
-            if len(row) >= 3:
-                col_to_check = 2
-            else:
-                col_to_check = 0
-
-            for match in f1r:
-                if match.find(row[col_to_check]) != -1:
-                    sl = [row[i] for i in range(len(row))]
-                    f3w.append(sl)
-                    print(sl)
-                    list_found.append(sl)
-                    break
-                else:
-                    continue
-
-        f1.close()
-
-    f2.close()
-    if list_delete:
-        for i in list_delete:
-            os.remove(i+r'/document.mqxliff')
-        sleep(0.01)
-        for i in list_delete:
-            try:
-                os.rmdir(i)
-            except:
-                sleep(0.05)
-                os.rmdir(i)
-
-    if list_found:
-        f3w.append(['Summary'])
-        print('\nSummary')
-        set_found = {str(i) for i in list_found}
-        list_reduced = list(set_found)
-        for i in list_reduced:
-            f3w.append(str_to_ls(i))
-            print(i)
-
-        fn3 = var_export.get()
-        f3 = open(fn3, 'a', encoding='utf-8')
-        f3wc = csv.writer(f3, lineterminator='\n')
-        f3wc.writerows(f3w)
-        f3.close()
-
-        print('\n'+var_export.get().rsplit('/')[-1].rsplit('\\')[-1]+' was successfully created.\nPress Enter key to exit.')
-    else:
-        print('\nNo forbidden term was found!\nPress Enter key to exit.')
-
-    try:
-        input('\n')
-    except:
-        pass
-
-    tk_F.quit()
-
-
 def run(self):
     if btn_run['state'] == 'normal' or 'active':
-        check()
+        cf_scripts.check(tk_F, var_bl, var_csv, var_export)
 
 btn_run = tkinter.Button(text='Run', state='disabled')
 btn_run.grid(row=3, column=3, sticky=tkinter.E, padx=15, pady=5)
@@ -194,4 +98,3 @@ for i in three_vars:
 top = tk_F.winfo_toplevel()
 top.resizable(False, False)
 tk_F.mainloop()
-
