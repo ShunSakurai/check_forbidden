78fe5a205d2a79922be7da69116f6bd32a5445d5
ShunSakurai <justmyway.yui@gmail.com>
2016-04-23 23:24:59 +0900

    Reduce the size of the .exe file

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
+'''
+cd dropbox/codes/check_forbidden
+py -3.4 setup.py py2exe
+
+Libraries used:
+import tkinter
+import tkinter.filedialog
+import csv
+import os
+import re
+from time import sleep
+import zipfile
+'''
 from distutils.core import setup
 import py2exe
 
 setup(
-      console=[{'script': 'check_forbidden.py', 'version': '1.3.0', }],
-      options={'py2exe': {'bundle_files': 2}}
-      )
-
-'''
-cd dropbox/codes/check_forbidden
-py -3.4 setup.py py2exe
-'''
+      console=[{'author': 'Shun Sakurai',
+                       'script': 'check_forbidden.py',
+                       'version': '1.4.0',
+                       }],
+      options={'py2exe': {
+                     'bundle_files': 2,
+                     'compressed': True,
+                     'excludes': ['_hashlib', '_frozen_importlib', 'argparse', '_lzma', '_bz2', '_ssl', 'calendar', 'datetime', 'difflib', 'doctest', 'inspect', 'locale', 'optparse', 'pdb', 'pickle', 'pydoc', 'pyexpat', 'pyreadline'],
+                     }}
+     )
