2908fc4c3132bc51094b4439ddbcbd79f239e9b6
ShunSakurai <justmyway.yui@gmail.com>
2016-04-28 13:15:13 +0900

    Assign the return (enter) key each widget's event

--- check_forbidden.py
+++ check_forbidden.py
@@ -19,9 +19,9 @@ btn_bl = tkinter.Button(text='Billingual', underline=0)
 btn_terms = tkinter.Button(text='Terms', underline=0)
 btn_result = tkinter.Button(text='Result', underline=0)
 
-btn_bl.grid(row=0, column=0, sticky=tkinter.W, padx=5)
-btn_terms.grid(row=1, column=0, sticky=tkinter.W, padx=5)
-btn_result.grid(row=2, column=0, sticky=tkinter.W, padx=5)
+three_buttons = [btn_bl, btn_terms, btn_result]
+for btn in three_buttons:
+    btn.grid(row=three_buttons.index(btn), column=0, sticky=tkinter.W, padx=5)
 
 var_bl = tkinter.StringVar(frame_main)
 var_terms = tkinter.StringVar(frame_main)
@@ -33,7 +33,7 @@ ent_result = tkinter.Entry(width=85, textvariable=var_result)
 
 
 def focus_off():
-    btn_bl.focus_set()
+    label_guide.focus_set()
 
 three_entries = [ent_bl, ent_terms, ent_result]
 for ent in three_entries:
@@ -60,17 +60,19 @@ def choose_result(self):
     var_result.set(f_result)
     focus_off()
 
-btn_bl.bind('<ButtonRelease-1>', choose_bl)
-btn_terms.bind('<ButtonRelease-1>', choose_terms)
-btn_result.bind('<ButtonRelease-1>', choose_result)
+func_buttons = [choose_bl, choose_terms, choose_result]
+for i in range(3):
+    three_buttons[i].bind('<ButtonRelease-1>', func_buttons[i])
 
-guide_bl = 'Billingual files: .mqxlz or .mqxliff'
-guide_terms = 'CSV format: 0(Index), Source, Target (NG), Target (OK)'
-guide_result = 'Can be an existing file. Results are added to the bottom.'
-guide_options = 'Show / hide options'
-guide_run = 'Run button is enabled when all the three fields are filled.'
 label_guide = tkinter.Label(text='')
 label_guide.grid(row=3, column=1, sticky=tkinter.W)
+
+guide_bl = '.mqxlz or .mqxliff'
+guide_terms = 'Text or CSV: Index, Source, Target (NG), Target (OK)'
+guide_result = 'Can be an existing file. Results are added to the bottom.'
+guide_options = 'Show or hide options.'
+guide_run = 'Enabled when all the three fields are filled.'
+guides_buttons = [guide_bl, guide_terms, guide_result]
 
 
 def show_guide(self, guide):
@@ -81,35 +83,10 @@ def hide_guide(self):
     label_guide['text'] = ''
 
 btn_bl.bind('<Enter>', lambda x: show_guide('<Enter>', guide_bl))
-btn_bl.bind('<Leave>', hide_guide)
 btn_terms.bind('<Enter>', lambda x: show_guide('<Enter>', guide_terms))
-btn_terms.bind('<Leave>', hide_guide)
 btn_result.bind('<Enter>', lambda x: show_guide('<Enter>', guide_result))
-btn_result.bind('<Leave>', hide_guide)
-
-
-def run(self):
-    if btn_run['state'] == 'active' or btn_run['state'] == 'normal':
-        cf_scripts.check(frame_main, var_bl.get(), var_terms.get(), var_result.get(), var_rate.get(), var_locked.get())
-
-btn_run = tkinter.Button(text='Run', state='disabled')
-btn_run.grid(row=3, column=2, sticky=tkinter.E, padx=15, pady=5)
-btn_run.bind('<ButtonRelease-1>', run)
-btn_run.bind('<Enter>', lambda x: show_guide('<Enter>', guide_run))
-btn_run.bind('<Leave>', hide_guide)
-
-
-def true_false(var, unknown, w):
-    if var_bl.get() and var_terms.get() and var_result.get():
-        btn_run['state'] = 'normal'
-        btn_run['text'] = 'Run!'
-    else:
-        btn_run['state'] = 'disabled'
-        btn_run['text'] = 'Run'
-
-three_vars = [var_bl, var_terms, var_result]
-for i in three_vars:
-    i.trace('w', true_false)
+for i in range(3):
+    three_buttons[i].bind('<Leave>', hide_guide)
 
 frame_options = tkinter.Frame(root, pady=5)
 frame_options.grid(row=4, column=1, sticky=tkinter.W)
@@ -119,16 +96,23 @@ label_rates.grid(row=0, column=0, sticky=tkinter.W)
 label_locked = tkinter.Label(frame_options, text='\tLocked status')
 label_locked.grid(row=0, column=1, sticky=tkinter.W)
 
+
+def select_and_focus(self):
+    self.widget.select()
+    self.widget.focus()
+
 match_rates = [('Check all segments', 'all', 6),
-                           ('Exclude 101% matches', '101', 10),
-                           ('Exclude 100% and 101 %', '100', 10)]
+                           (r'Exclude 101% matches', '101', 10),
+                           (r'Exclude 100% and 101%', '100', 10)]
 var_rate = tkinter.StringVar()
 var_rate.set('all')
 rbs_rate = []
 for label, rate, ul in match_rates:
     rb_rate = tkinter.Radiobutton(frame_options, text=label, variable=var_rate, value=rate, underline=ul)
-    rb_rate.grid(row=match_rates.index((label, rate, ul)) + 1, column=0, sticky=tkinter.W, padx=10)
     rbs_rate.append(rb_rate)
+for rb in rbs_rate:
+    rb.grid(row=(rbs_rate.index(rb) + 1), column=0, sticky=tkinter.W, padx=10)
+    rb.bind('<ButtonRelease-1>', select_and_focus)
 
 locked_states = [('Include locked segments', 'all', 0),
                               ('Exclude locked segments', 'locked', 0)]
@@ -137,8 +121,10 @@ var_locked.set('all')
 rbs_locked = []
 for label, state, ul in locked_states:
     rb_locked = tkinter.Radiobutton(frame_options, text=label, variable=var_locked, value=state, underline=ul)
-    rb_locked.grid(row=locked_states.index((label, state, ul)) + 1, column=1, sticky=tkinter.W, padx=10)
     rbs_locked.append(rb_locked)
+for rb in rbs_locked:
+    rb.grid(row=(rbs_locked.index(rb) + 1), column=1, sticky=tkinter.W, padx=10)
+    rb.bind('<ButtonRelease-1>', select_and_focus)
 
 
 def show_hide_options(self, widget):
@@ -158,6 +144,30 @@ btn_options.bind('<Enter>', lambda x: show_guide('<Enter>', guide_options))
 btn_options.bind('<Leave>', hide_guide)
 
 
+def run(self):
+    if btn_run['state'] == 'active' or btn_run['state'] == 'normal':
+        cf_scripts.check(frame_main, var_bl.get(), var_terms.get(), var_result.get(), var_rate.get(), var_locked.get())
+
+btn_run = tkinter.Button(text='Run', state='disabled', takefocus=True)
+btn_run.grid(row=3, column=2, sticky=tkinter.E, padx=15, pady=5)
+btn_run.bind('<ButtonRelease-1>', run)
+btn_run.bind('<Enter>', lambda x: show_guide('<Enter>', guide_run))
+btn_run.bind('<Leave>', hide_guide)
+
+
+def enable_run_if_filled(var, unknown, w):
+    if var_bl.get() and var_terms.get() and var_result.get():
+        btn_run['state'] = 'normal'
+        btn_run['text'] = 'Run!'
+    else:
+        btn_run['state'] = 'disabled'
+        btn_run['text'] = 'Run'
+
+three_vars = [var_bl, var_terms, var_result]
+for i in three_vars:
+    i.trace('w', enable_run_if_filled)
+
+
 def run_when_out_of_ent(func):
     if type(frame_main.focus_get()) == tkinter.Entry:
         pass
@@ -168,7 +178,6 @@ def run_when_out_of_ent(func):
 def bind_keys(key, func):
     root.bind(key, lambda x: run_when_out_of_ent(func))
 
-bind_keys('<Return>', run)
 bind_keys('<space>', run)
 bind_keys('o', lambda y: show_hide_options('o', btn_options))
 bind_keys('b', choose_bl)
@@ -179,6 +188,12 @@ bind_keys('1', lambda y: rbs_rate[1].select())
 bind_keys('0', lambda y: rbs_rate[2].select())
 bind_keys('i', lambda y: rbs_locked[0].select())
 bind_keys('e', lambda y: rbs_locked[1].select())
+
+
+def return_to_click(self):
+    frame_main.focus_get().event_generate('<ButtonRelease-1>')
+
+root.bind('<Return>', return_to_click)
 
 top = frame_main.winfo_toplevel()
 top.resizable(False, False)
