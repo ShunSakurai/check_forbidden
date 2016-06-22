e4f80de97931cad35269104a04170c04217b389a
ShunSakurai <justmyway.yui@gmail.com>
2016-06-22 14:57:31 +0900

    Display the version number

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 '''
 cd dropbox/codes/check_forbidden
-py -3.4 setup.py py2exe
 
 rmdir /s __pycache__
 rmdir /s dist
+
+py -3.4 setup.py py2exe
 
 Libraries used:
 import tkinter
@@ -16,20 +17,26 @@ from time import sleep
 import zipfile
 import doctest
 '''
-from distutils.core import setup
-import py2exe
 
-setup(
-    console=[{
-        'author': 'Shun Sakurai',
-        'dest_base': 'Check Forbidden',
-        'icon_resources': [(1, './icons/check_forbidden_icon.ico')],
-        'script': 'check_forbidden.py',
-        'version': '1.5.6',
-    }],
-    options={'py2exe': {
-        'bundle_files': 2,
-        'compressed': True,
-        'excludes': ['_bz2', '_hashlib', '_frozen_importlib', '_lzma', '_ssl'  'argparse', 'calendar', 'difflib', 'doctest' 'inspect', 'locale', 'optparse', 'pdb', 'pickle', 'pydoc', 'pyexpat', 'pyreadline'],
-    }}
-)
+dict_console = {
+    'author': 'Shun Sakurai',
+    'dest_base': 'Check Forbidden',
+    'icon_resources': [(1, './icons/check_forbidden_icon.ico')],
+    'script': 'check_forbidden.py',
+    'version': '1.5.8',
+}
+
+dict_options = {
+    'bundle_files': 2,
+    'compressed': True,
+    'excludes': ['_bz2', '_hashlib', '_frozen_importlib', '_lzma', '_ssl'  'argparse', 'calendar', 'difflib', 'doctest' 'inspect', 'locale', 'optparse', 'pdb', 'pickle', 'pydoc', 'pyexpat', 'pyreadline']
+}
+
+if __name__ == "__main__":
+    from distutils.core import setup
+    import py2exe
+
+    setup(
+        console=[dict_console],
+        options={'py2exe': dict_options}
+    )
